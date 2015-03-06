331585ff471591d7a04f9f5c18002534b8884309
Marc-Alexandru Baetica <marc.baetica@gmail.com>
2015-03-06 15:54:23 -0500

    updated Database

--- SQLdump.sql
+++ SQLdump.sql
@@ -6,16 +6,16 @@ drop table if exists macrulestable;
 
 CREATE TABLE IF NOT EXISTS `macRulesTable` (
 `Index` integer PRIMARY KEY AUTO_INCREMENT NOT NULL,
-`mac` bigint(228) NOT NULL,				#was bigint(228) but temporarily set as varchar(17) for testing purposes with the GUI
+`mac` bigint(228) NOT NULL, #was bigint(228) but temporarily set as varchar(17) for testing purposes with the GUI
 `block` tinyint(1) DEFAULT '0',
-`user_total` bigint(9) DEFAULT '1',		#new user should get a maximum of 1GB of total allocaiton for personal user
-`total_all` bigint(9) DEFAULT '60',		#to be updated when user generates initial setup
-`start_time` time DEFAULT '12:00:00',	#start and stop time are set to a default 24h cycle
+`user_total` bigint(9) DEFAULT '20', #new user should get a maximum of 1GB of total allocaiton for personal user
+`total_all` bigint(9) DEFAULT '60', #to be updated when user generates initial setup
+`start_time` time DEFAULT '12:00:00', #start and stop time are set to a default 24h cycle
 `stop_time` time DEFAULT '12:00:00',
 `bw_limit` bigint(10) DEFAULT '4294967294'	#set for maximum rate in kb/s
-`current_user_usage` #the usage per user
-`current_total_usage` #the total usage over the internet duplicate for every user
-`terminal_name` #string for GUI render without 
+`current_user_usage` bigint(10) DEFAULT '15' #the usage per user
+`current_total_usage` bigint(10) DEFAULT '20' #the total usage over the internet duplicate for every user
+`terminal_name` varchar(17) DEFAULT NULL #string for rendering the computer names in the GUI instead of the MAC address
 );
 
 #temporarily used for testing purposes. Make sure to comment out in the final version
